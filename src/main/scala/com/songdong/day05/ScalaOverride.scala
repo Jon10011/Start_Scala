f2c01b22419e4817463c4a65805f37124dc8693b
Jon <dong_song1@163.com>
2019-07-03 22:35:10 +0800

    scala 覆写字段动态绑定

--- src/main/scala/com/songdong/day05/ScalaOverride.scala
+++ src/main/scala/com/songdong/day05/ScalaOverride.scala
@@ -1,11 +1,17 @@
 package com.songdong.day05
 
 object ScalaOverride {
+  /**
+    * 1、def只能重写另一个def
+    * 2、val只能重写另外一个val属性或者重写不带参数的def
+    *
+    * @param args
+    */
   def main(args: Array[String]): Unit = {
     val obj1: A1 = new B1
     val obj2: B1 = new B1
     //动态绑定
-    println("obj1.age="+obj1.age+"\tobj2.age="+obj2.age)
+    println("obj1.age=" + obj1.age + "\tobj2.age=" + obj2.age)
   }
 }
 
